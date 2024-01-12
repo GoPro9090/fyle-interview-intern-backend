f62b97f78fda5bbbdcb37b7de74a185ffa0b286d
Madan Gopal <madangopal16072000@gmail.com>
2024-01-12 19:44:34 +0530

    second version

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -56,6 +56,9 @@ class Assignment(db.Model):
             assignment = assignment_new
             db.session.add(assignment_new)
 
+        #content cannot be null
+        assertions.assert_valid(assignment.content is not None, "content cannot be null")
+
         db.session.flush()
         return assignment
 
@@ -65,8 +68,10 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, "only a draft assignment can be submitted")
 
         assignment.teacher_id = teacher_id
+        assignment.state = AssignmentStateEnum.SUBMITTED
         db.session.flush()
 
         return assignment
@@ -77,6 +82,18 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+
+        # Check the role of the authenticated principal
+        if auth_principal.principal_id is not None:
+            # For principal, the state of the assignment must not be DRAFT
+            assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'Only submitted or graded assignments can be marked by the principal')
+        elif auth_principal.teacher_id is not None:
+            # For teacher, the state of the assignment must be SUBMITTED
+            assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED, 'Only submitted assignments can be graded by the teacher')
+            assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, "Only teacher whom assignments have been submitted can grade")
+        else:
+            # If neither principal nor teacher, raise an assertion
+            assertions.assert_true(False, 'Invalid role for marking the grade')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
@@ -91,3 +108,9 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_teacher(cls, teacher_id):
         return cls.filter(cls.teacher_id == teacher_id).all()
+
+    @classmethod
+    def get_assignments_by_principal(cls):
+        # Filter assignments with state "submitted" or "graded"
+        desired_states = (AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED)
+        return cls.filter(cls.state.in_(desired_states)).all()
--- tests/principals_test.py
+++ tests/principals_test.py
@@ -60,3 +60,21 @@ def test_regrade_assignment(client, h_principal):
 
     assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
     assert response.json['data']['grade'] == GradeEnum.B
+
+
+def test_get_teachers(client, h_principal):
+    response = client.get(
+        '/principal/teachers',
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
+
+def test_ready(client):
+    response = client.get(
+        '/',
+    )
+
+    assert response.status_code == 200
+    data = response.json
+    assert data['status'] == "ready"
